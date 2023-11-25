16d9a478e465b479475a39c94f9a880a7c9e9266
roxxel <warperthe@gmail.com>
2023-11-25 15:25:35 +0200

    images proxy

--- src/components/mdrenderer.tsx
+++ src/components/mdrenderer.tsx
@@ -1,59 +1,69 @@
-'use client'
+"use client";
 import remarkGfm from "remark-gfm";
 import rehypeCodeTitles from "rehype-code-titles";
 //@ts-ignore
 import rehypeFigure from "rehype-figure";
 import rehypePrismAll from "rehype-prism-plus";
-import rehypeVideo from 'rehype-video'
+import rehypeVideo from "rehype-video";
 import Markdown from "react-markdown";
 import rehypeHighlight from "rehype-highlight";
 import Head from "next/head";
+import Image from "next/image";
 import hljs from "highlight.js";
 import { useLayoutEffect } from "react";
 import exportAsImage from "@/lib/utils";
+import ImageWithFallback from "./ui/imagewithfallback";
 export default function MDRenderer({
   markdown,
   className,
-  displayScreenshotButton
+  displayScreenshotButton,
 }: {
   markdown: string;
   className: string;
-  displayScreenshotButton?: boolean
+  displayScreenshotButton?: boolean;
 }) {
   if (markdown.startsWith("CODE")) {
-    const lang = markdown.split("\n")[0].replaceAll("CODE:", "").trim()
-    markdown = '```' + lang + '\n' + markdown.split("\n").slice(1).join('\n') + "\n```"
+    const lang = markdown.split("\n")[0].replaceAll("CODE:", "").trim();
+    markdown =
+      "```" + lang + "\n" + markdown.split("\n").slice(1).join("\n") + "\n```";
   }
 
   useLayoutEffect(() => {
     // If render happened on server - do nothing return
-    if (typeof window === 'undefined') return
-    let btns: HTMLButtonElement[] = []
+    if (typeof window === "undefined") return;
+    let btns: HTMLButtonElement[] = [];
     const screenshot = async (e: MouseEvent) => {
-      await exportAsImage(document.querySelector(`pre[data-id='${(e.target as HTMLElement).dataset['preid']}']`)!)
-    }
+      await exportAsImage(
+        document.querySelector(
+          `pre[data-id='${(e.target as HTMLElement).dataset["preid"]}']`
+        )!
+      );
+    };
     if (displayScreenshotButton) {
-      const pres = [...Array.from(document.querySelectorAll('pre'))]
-      pres.forEach((x,i)=> {
+      const pres = [...Array.from(document.querySelectorAll("pre"))];
+      pres.forEach((x, i) => {
         //@ts-ignore
-        x.style = "position: relative"
-        x.dataset['id'] = i.toString()
-        x.classList.add('pre-custom')
-        const button = document.createElement('button')
-        button.innerText = "Save as image"
-        button.classList.add('pre-btn')
-        button.dataset['preid'] = i.toString()
+        x.style = "position: relative";
+        x.dataset["id"] = i.toString();
+        x.classList.add("pre-custom");
+        const button = document.createElement("button");
+        button.innerText = "Save as image";
+        button.classList.add("pre-btn");
+        button.dataset["preid"] = i.toString();
         //@ts-ignore
-        button.style = "position:absolute; top: 12px; right: 12px;"
-        x.appendChild(button)
-        btns.push(button)
-        button.addEventListener('click', screenshot)
-      })
+        button.style = "position:absolute; top: 12px; right: 12px;";
+        x.appendChild(button);
+        btns.push(button);
+        button.addEventListener("click", screenshot);
+      });
     }
     return () => {
-      btns.forEach(x=>{ x.removeEventListener('click', screenshot); x.parentNode!.removeChild(x) })
-    }
-  }, [markdown])
+      btns.forEach((x) => {
+        x.removeEventListener("click", screenshot);
+        x.parentNode!.removeChild(x);
+      });
+    };
+  }, [markdown]);
   try {
     return (
       <>
@@ -70,12 +80,36 @@ export default function MDRenderer({
             `}
         </style>
         <div className={`markdown-body ${className}`}>
-          <Markdown 
-          components={{p(props) {
-            const {node, ...rest} = props
-            return <p style={{fontSize: '14px'}} {...rest} />
-          }}}
-          rehypePlugins={[rehypeHighlight, remarkGfm, [rehypeVideo, {details: false}]]}>
+          <Markdown
+            components={{
+              p(props) {
+                const { node, ...rest } = props;
+                return <p style={{ fontSize: "14px" }} {...rest} />;
+              },
+              img(props) {
+                const { node, src, width, height, placeholder, ref, className, ...rest } =
+                  props;
+                return (
+                  <div className="relative left-0 h-[300px] ">
+                    <ImageWithFallback
+                      fallbackSrc={"/noimage.svg"}
+                      fill={true}
+                      src={src || "/not-found"}
+                      alt=""
+                      style={{backgroundColor: 'rgb(30 41 59) !important'}}
+                      className={`${className || ''} min-w-screen`}
+                      {...rest}
+                    />
+                  </div>
+                );
+              },
+            }}
+            rehypePlugins={[
+              rehypeHighlight,
+              remarkGfm,
+              [rehypeVideo, { details: false }],
+            ]}
+          >
             {markdown}
           </Markdown>
         </div>
