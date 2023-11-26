1d64ddf79e8c5006087c9a035896c687c6fd3caa
roxxel <warperthe@gmail.com>
2023-11-26 14:51:19 +0200

    well...

--- src/app/paste/[id]/docx/route.ts
+++ src/app/paste/[id]/docx/route.ts
@@ -1,20 +1,20 @@
-import { rxdb } from "@/lib/mongo";
+// import { rxdb } from "@/lib/mongo";
 import { NextRequest, NextResponse } from "next/server";
-import crypto from 'crypto'
-import { ObjectId } from "mongodb";
-const mark = require('markdocx')
-const {markdocx}: any = mark
+// import crypto from 'crypto'
+// import { ObjectId } from "mongodb";
+// const mark = require('markdocx')
+// const {markdocx}: any = mark
 
 
 
 export async function GET(request: NextRequest, {params: {id}}: {params: {id: string}}) {
-    const db = await rxdb();
-    const pastes = await db.collection("pastes");
-    const paste = await pastes.findOne({ _id: new ObjectId(id) });
-    if (!paste) {
-        return new NextResponse('NOT_FOUND', {status: 404})
-    }
-    const buffer = await markdocx(paste!.paste)
+//     const db = await rxdb();
+//     const pastes = await db.collection("pastes");
+//     const paste = await pastes.findOne({ _id: new ObjectId(id) });
+//     if (!paste) {
+//         return new NextResponse('NOT_FOUND', {status: 404})
+//     }
+//     const buffer = await markdocx(paste!.paste)
 
-    return new NextResponse(buffer, {status: 200, headers: {'Content-Type': 'application/msword', 'Content-Disposition': 'attachment'}})
-}
+    return NextResponse.json({message: 'another time maybe'})
+ }
